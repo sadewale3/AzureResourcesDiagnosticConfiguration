03f37f86f615c4e57a4c5ec5c1d9df510ab3b62f
Samson Adewale <xangobolt@gmail.com>
2021-04-07 08:36:29 -0400

    first commit

---
+++ azure_FileService_DiagnosticSetting.ps1
@@ -0,0 +1,17 @@
+$saList = Get-AzStorageAccount | Select-Object storageAccountName, resourceGroupName, @{l='subcriptionID';e={$_.id.split('/')[2]}}
+
+$LAWS = $args[0]
+$LAWS
+
+foreach ($storageItem in $saList){
+    $subscription = $storageItem | ForEach-Object { $_.subcriptionID }
+    $resourceGroup = $storageItem | ForEach-Object { $_.resourceGroupName }
+    $storageAccount = $storageItem | ForEach-Object { $_.storageAccountName }
+    $resourceID = "/subscriptions/"+$subscription+"/resourceGroups/"+$resourceGroup+"/providers/Microsoft.Storage/storageAccounts/"+$storageAccount+"/fileServices/default"
+    $DiagSettingName = $storageAccount+"-fileservice-diag-01"
+    
+    Set-AzDiagnosticSetting -ResourceId $resourceID -Name $DiagSettingName -WorkspaceId $LAWS -Enabled $true -Category StorageWrite, StorageRead, StorageDelete
+    
+    $Output = "File Service Diagnostic Setting has been enabled for "+$storageAccount+" Storage Account"
+    $Output
+}
---
+++ azure_QueueService_DiagnosticSetting.ps1
@@ -0,0 +1,17 @@
+$saList = Get-AzStorageAccount | Select-Object storageAccountName, resourceGroupName, @{l='subcriptionID';e={$_.id.split('/')[2]}}
+
+$LAWS = $args[0]
+$LAWS
+
+foreach ($storageItem in $saList){
+    $subscription = $storageItem | ForEach-Object { $_.subcriptionID }
+    $resourceGroup = $storageItem | ForEach-Object { $_.resourceGroupName }
+    $storageAccount = $storageItem | ForEach-Object { $_.storageAccountName }
+    $resourceID = "/subscriptions/"+$subscription+"/resourceGroups/"+$resourceGroup+"/providers/Microsoft.Storage/storageAccounts/"+$storageAccount+"/queueServices/default"
+    $DiagSettingName = $storageAccount+"-queueservice-diag-01"
+    
+    Set-AzDiagnosticSetting -ResourceId $resourceID -Name $DiagSettingName -WorkspaceId $LAWS -Enabled $true -Category StorageWrite, StorageRead, StorageDelete
+    
+    $Output = "Queue Service Diagnostic Setting has been enabled for "+$storageAccount+" Storage Account"
+    $Output
+}
